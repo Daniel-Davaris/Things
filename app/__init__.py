6107a3c271d7bd9036270b62da44418fb3c9b892
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-14 22:54:49 +1100

    moved static

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,static_folder=os.path.abspath('static'))
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
 
 
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'dev.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	
