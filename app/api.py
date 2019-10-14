691a860659db261df1052ff9e84ce0ea3109121d
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-14 23:47:33 +1100

    fixed formatting on add product and started working with zinc to add products

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app import db
 from app.models import (
     Item,
@@ -10,6 +10,10 @@ from app.models import (
     Brand_Item
 )
 import json
+import requests
+from keys import api_key
+
+zinc = 'https://api.zinc.io/v1/'
 
 api = Blueprint('API', __name__)
 
@@ -35,22 +39,35 @@ def product(productID):
     }
     
     """
-    item  = Item.query.filter_by(product_id=productID).first_or_404()
-    imgs  = sorted(Image.query.filter_by(item_id=item.id).all(), key=lambda x: x.is_primary)
-    cats  = [Category.get(x.category_id) for x in Category_Item.query.filter_by(item_id=item.id).all()]
-    brand = [Brand.query.get(x.brand_id) for x in Brand_Item.query.filter_by(item_id=item.id)]
+    item    = Item.query.filter_by(product_id=productID).first_or_404()
+    imgs    = sorted(Image.query.filter_by(item_id=item.id).all(), key=lambda x: x.is_primary)
+    cats    = [Category.get(x.category_id) for x in Category_Item.query.filter_by(item_id=item.id).all()]
+    brand   = [Brand.query.get(x.brand_id) for x in Brand_Item.query.filter_by(item_id=item.id)]
     details = Details.query.filter_by(item_id=item.id)
 
     return json.dumps({
-        'id':item.id,
+        'id'        :item.id,
         'product_id':item.product_id,
-        'title':item.title,
-        'desc':item.desc,
-        'details': details,
-        'bullets': item.bullets,
-        'brand':brand,
-        'imgs':imgs,
+        'title'     :item.title,
+        'desc'      :item.desc,
+        'details'   :details,
+        'bullets'   :item.bullets,
+        'brand'     :brand,
+        'imgs'      :imgs,
         'categories':cats,
-        'old_price':item.old_price,
-        'new_price':item.new_price,
+        'old_price' :item.old_price,
+        'new_price' :item.new_price,
     })
+
+@api.route('/addProduct', methods=['POST'])
+def addProduct():
+    try:
+        data = request.get_json()
+
+        data = requests.get(
+            f"{zinc}/products/{data['product_id']}",
+            params=[('retailer', retailer)],
+            auth=(',client_token>', api_key)
+        )
+    except:
+        return "Invalid input format", 500
--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home')
 def home():
