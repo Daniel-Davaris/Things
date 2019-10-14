bfe79cdef103e271f7152a7a0c25c98467cc57b3
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-14 22:43:47 +1100

    added heaps of models

--- app/models.py
+++ app/models.py
@@ -20,12 +20,59 @@ class Person(db.Model, UserMixin):
     def checkPass(self, password):
         return check_password_hash(self.pass_hash, password)
 
+
 class Item(db.Model):
+    __tablename__ = 'item'
 
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.)
-    img = db.Column(db.Blob)
+    product_id = db.Column(db.String(20))
     title = db.Column(db.String(50))
     desc = db.Column(db.String(300))
+    bullets = db.Column(db.String(100))
+    old_price = db.Column(db.Integer)
+    new_price = db.Column(db.Integer)
 
 
+
+class Image(db.Model):
+    __tablename__ = 'image'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer)
+    img_url = db.Column(db.String(100))
+    is_primary = db.Column(db.Boolean)
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(100)
+
+
+class Category_Item(db.Model):
+    __tablename__ = 'category_item'
+    item_id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, primary_key=True)
+
+
+class Brand(db.Model):
+    __tablename__ = 'brand'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(100)
+
+
+class Brand_Item(db.Model):
+    __tablename__ = 'brand_item'
+
+    item_id = db.Column(db.Integer, primary_key=True)
+    brand_id = db.Column(db.Integer, primary_key=True)
+
+
+class Details(db.Model):
+    __tablename__ = 'details'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer)
+    text = db.Column(db.String(200))
