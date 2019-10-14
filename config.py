43daf1958b0aea76e1c38f320fb0106c6ce2b34c
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-14 22:47:43 +1100

    fixed a few things

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 """Configuration for flask app."""
 import os
-import creds
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -8,9 +7,10 @@ class Config(object):
 	"""Object containing all of the config data for the app"""
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
 
-	SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s'\
-		% creds.POSTGRES
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'dev.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	
 	# ! DISABLE IN PROD
 	DEBUG = True
